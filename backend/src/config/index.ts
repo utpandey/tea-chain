9ef6f390413902191c69b69b801759abe9d3b376
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2021-05-11 00:17:10 +0530

    feat: add docker config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,6 +5,11 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   databaseURL: process.env.DATABASE_URI,
+  databaseUser: process.env.DATABASE_USERNAME,
+  databasePass: process.env.DATABASE_PASSWORD,
+  databaseHost: process.env.DATABASE_HOST,
+  databasePort: process.env.DATABASE_PORT,
+  databaseDb: process.env.DATABASE_DB,
   emailUser: process.env.EMAIL_USER,
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -10,7 +10,7 @@ export default () => {
     useUnifiedTopology: true,
     useFindAndModify: false,
   };
-  const connectionUri = serverConfig.databaseURL || '';
+  const connectionUri = `mongodb://${serverConfig.databaseUser}:${serverConfig.databasePass}@${serverConfig.databaseHost}:${serverConfig.databasePort}/${serverConfig.databaseDb}?authSource=admin`;
   mongoose.connect(connectionUri, connectionOptions)
     .then(() => logger.info(NAMESPACE, 'Connection to database has been established'))
     .catch((error) => logger.error(NAMESPACE, 'Connection could not be established', error));

14427b1ab92bcf685ea87c2c982a70ccd3729ff8
utpandey <utsavpandey8@gmail.com>
2021-05-20 23:55:33 +0530

    feat: modals added for all roles

---
+++ frontend/src/components/Modals/index.tsx
@@ -0,0 +1,5 @@
+export { FarmerModal } from "./FarmerModal";
+export { DistributerModal } from "./DistributerModal";
+export { ManufactureModal } from "./ManufactureModal";
+export { WholesaleModal } from "./WholesaleModal";
+export { RetailModal} from "./RetailModal";

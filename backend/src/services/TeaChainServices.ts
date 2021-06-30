41676576ba6d36b7e4443a875ee2e7cb48d91f29
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2021-06-30 21:21:00 +0530

    feat: update tea data route

--- backend/src/services/TeaChainServices.ts
+++ backend/src/services/TeaChainServices.ts
@@ -1,6 +1,24 @@
-const retrieveData = (id: string) => {
-  if (id) return true;
-  return false;
+import { ethers } from 'ethers';
+import Teachain from '../artifacts/contracts/Teachain.sol/Teachain.json';
+import logger from '../config/logger';
+import serverConfig from '../config';
+
+const retrieveData = async (id: string) => {
+  // eslint-disable-next-line new-cap
+  const provider = new (ethers.providers.getDefaultProvider as any)(serverConfig.providerAddress);
+  const contract = new ethers.Contract(serverConfig.contractAddress || '', Teachain.abi, provider);
+  try {
+    const data = await contract.functions.getTeaBatch(parseInt(id, 10));
+    const response = data[0];
+    logger.info('TEACHAIN', 'retrieve data', response);
+    if (response[1].isVerified === false) {
+      return false;
+    }
+    return response;
+  } catch (err) {
+    logger.error('TEACHAIN', 'retrieve data', err);
+    return false;
+  }
 };
 
 export default { retrieveData };
